29e393a6ad4c19c07e0452433a1007e058568a1f
RiteshBarapatre <riteshbarapatre543@gmail.com>
2023-05-24 14:54:14 +0530

    Little change in backend

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL : 'https://face-gram-n7bxjxhpg-riteshbarapatre543-gmailcom.vercel.app/'
+    baseURL : 'http://localhost:8000'
 })
 
 export default instance
